4373a3635b34165cc8e3f6b848db88138346f493
snyder <snyderjosephd@yahoo.com>
2019-10-03 20:56:51 -0500

    Completed List exercises

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace lists
 {
@@ -6,7 +7,47 @@ namespace lists
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> planetList = new List<string>(){ "Mercury", "Mars" };
+
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+            List<string> lastPlanets = new List<string>(){ "Uranus", "Neptune" };
+
+            planetList.AddRange(lastPlanets);
+
+            planetList.Insert(1, "Venus");
+            planetList.Insert(2, "Earth");
+            planetList.Add("Pluto");
+
+            // string[] output = dinosaurs.GetRange(2, 3).ToArray();
+
+            List<string> rockyPlanets = planetList.GetRange(0, 4);
+
+            // Console.WriteLine("planetList");
+            // foreach (string planet in planetList) 
+            // {
+            //     Console.WriteLine(planet);
+            // }
+
+            // Console.WriteLine("rockyPlanets");
+            // foreach(string planet in rockyPlanets)
+            // {
+            //     Console.WriteLine(planet);
+            // }
+
+            // planetList.Remove("Pluto");
+
+            // Console.WriteLine("planetList");
+            // foreach(string planet in planetList)
+            // {
+            //     Console.WriteLine(planet);
+            // }
+
+            // Print lists to the console
+            planetList.ForEach(Console.WriteLine);
+            rockyPlanets.ForEach(Console.WriteLine);
         }
     }
 }
+
