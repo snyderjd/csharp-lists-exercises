f0896e4f8f7c3cf39bdbcb24e68799cc2249dd4c
snyder <snyderjosephd@yahoo.com>
2019-10-04 08:54:32 -0500

    Completed Random Numbers exercise

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace lists
     {
         static void Main(string[] args)
         {
+            // ========== Practice: Planet and Spaceships ==========
             List<string> planetList = new List<string>(){ "Mercury", "Mars" };
 
             planetList.Add("Jupiter");
@@ -30,24 +31,32 @@ namespace lists
             //     Console.WriteLine(planet);
             // }
 
-            // Console.WriteLine("rockyPlanets");
-            // foreach(string planet in rockyPlanets)
-            // {
-            //     Console.WriteLine(planet);
-            // }
-
-            // planetList.Remove("Pluto");
-
-            // Console.WriteLine("planetList");
-            // foreach(string planet in planetList)
-            // {
-            //     Console.WriteLine(planet);
-            // }
-
             // Print lists to the console
-            planetList.ForEach(Console.WriteLine);
-            rockyPlanets.ForEach(Console.WriteLine);
+            // planetList.ForEach(Console.WriteLine);
+            // rockyPlanets.ForEach(Console.WriteLine);
+
+            // ========== Practice: Random Numbers ==========
+            Random random = new Random();
+
+            List<int> numbers = new List<int> {
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                random.Next(6)
+            };
+
+            for (int i = 0; i < numbers.Count; i++) {
+                // Determine if the current loop index is contained inside of the 'numbers' list. Print a message to the console indicating whether or not the index is on the list
+                if (numbers.Contains(i)) {
+                    Console.WriteLine($"numbers list contains {i}");
+                } else {
+                    Console.WriteLine($"numbers list does not contain {i}");
+                }
+            }
         }
     }
 }
+
 
